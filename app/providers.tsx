8a11a1dde28b99b52dd62339f39e30cc72d436f7
sqcscpoe <wilfredorios555@gmail.com>
2026-05-01 06:52:39 -0400

    SQC Scope: app/providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,3 @@
 'use client'
-import { SessionProvider } from 'next-auth/react'
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
-}
+import{SessionProvider}from'next-auth/react'
+export function Providers({children}:{children:React.ReactNode}){return<SessionProvider>{children}</SessionProvider>}
