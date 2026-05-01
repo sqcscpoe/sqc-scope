48ef99e966717640940ea2b68870464edcd0b11e
sqcscpoe <wilfredorios555@gmail.com>
2026-05-01 06:52:41 -0400

    SQC Scope: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
-export default async function Home() {
-  const session = await getServerSession(authOptions)
-  if (session) redirect('/dashboard')
-  redirect('/login')
-}
+import{redirect}from'next/navigation'
+import{getServerSession}from'next-auth'
+import{authOptions}from'./api/auth/[...nextauth]/route'
+export default async function Home(){
+const session=await getServerSession(authOptions)
+if(session)redirect('/dashboard')
+redirect('/login')}
